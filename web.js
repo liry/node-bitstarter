947e25248f64f1ae43df029c697516c7acded961
Li Ry <rysavylibor@gmail.com>
2013-10-05 23:41:11 +0200

    chg: set up dev/stg/prod flow

--- web.js
+++ web.js
@@ -3,12 +3,16 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+//static content - it would be better to serve it through nginx put before node.js
+process.env.PWD = process.cwd()
+app.use(express.static(process.env.PWD + '/assets'));
+
 app.get('/', function (request, response) {
     var fileBuff = fs.readFileSync('index.html');
     response.send(fileBuff.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function () {
     console.log("Listening on " + port);
 });
